78936540973a35c3375b9566b874f28a2fac2cbe
Kat Fay <101537105+katfay@users.noreply.github.com>
2023-03-05 17:09:23 +1100

    Set up local server

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ console.log("nodemon successfully started");
 const express = require("express");
 const app = express();
 const port = 3000;
-app.listen(3000, () => {
-  console.log("Server started on port 3000");
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
 });
