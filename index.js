f909c5128c7bd7314c84a135526b11843b386059
Kat Fay <101537105+katfay@users.noreply.github.com>
2023-03-05 17:04:59 +1100

    Add express listener

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 require("dotenv").config();
 
 console.log("nodemon successfully started");
+
+const express = require("express");
+const app = express();
+const port = 3000;
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
