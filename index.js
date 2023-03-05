71fb8e14b7a8dc95c942066bcf575e1082438abe
Kat Fay <101537105+katfay@users.noreply.github.com>
2023-03-06 08:45:33 +1100

    Update express functions

--- index.js
+++ index.js
@@ -7,13 +7,12 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+  // res.json(req.body);
+  res.send(`
+        <p>dogMoods local server</p>
+        <p>Click here to get access to the <a href="">Database</a></p>`);
 });
 
 app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
+  console.log(`dogMoods server started on port ${port}`);
 });
