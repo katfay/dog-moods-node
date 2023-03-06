2f4432a2d2e39fb213f4d3d4316bde59b64c7ee5
Kat Fay <101537105+katfay@users.noreply.github.com>
2023-03-06 14:03:37 +1100

    Create moods DB model controller

---
+++ models/moods.model.js
@@ -0,0 +1,29 @@
+//
+
+const mongoose = require("mongoose");
+
+var moodsSchema = new mongoose.Schema(
+  {
+    // id is inbuilt for MongoDB so is not required here
+    mood: {
+      type: String,
+      required: "This field is required",
+    },
+    date: {
+      type: date,
+      required: "This field is required",
+    },
+    notes: {
+      type: String,
+      required: "This field is required",
+    },
+    // The category is not specifically input by the user, but flows from choice of category (e.g. "mood" v "behaviour/action") - so this may not work, needs more experimentation
+    category: {
+      type: String,
+      required: "This field is required",
+    },
+  },
+  { versionKey: false }
+);
+
+mongoose.model("Mood", moodSchema);
