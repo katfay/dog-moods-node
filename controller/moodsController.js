9faf1c307546ffbb940c70a71f9a85e3051d5457
Kat Fay <101537105+katfay@users.noreply.github.com>
2023-03-06 14:54:52 +1100

    Complete moodsController.js

--- controller/moodsController.js
+++ controller/moodsController.js
@@ -11,7 +11,7 @@ const mongoose = require("mongoose");
 const Moods = mongoose.model("Moods");
 
 // Create an API endpoint ("/list")
-// Get the list of moods
+// Get the list of moods (the objects/data)
 // This URI should match the endpoint in the fetch function in the frontend JS file
 router.get("/list", (req, res) => {
   // Find the list of moods from (?the endpoint/the database collection)
@@ -30,6 +30,7 @@ router.get("/list", (req, res) => {
 });
 
 // Handles the ID key - which comes in automatically from MongoDB
+// Get the ID/s for the list of moods (data/objects)
 router.get("/:id", (req, res) => {
   Moods.findById(req.params.id, (err, doc) => {
     if (!err) {
@@ -42,3 +43,68 @@ router.get("/:id", (req, res) => {
     }
   });
 });
+
+// Post (insert or create and update) the objects/data
+// Note this endpoint is different to the "/list" endpoint
+router.post("/", (req, res) => {
+  // If the body ID does not exist or is blank, insert or create the record
+  if (!req.body_id || req.body._id == "") {
+    console.log("insert");
+    // Call custom function from below
+    insertRecord(req, res);
+    // Otherwise update the existing record
+  } else {
+    console.log("update");
+    // Call custom function from below
+    updateRecord(req, res);
+  }
+});
+
+// Custom function to insert (or create) a mood object
+function insertRecord(req, res) {
+  var moodsObj = new Moods();
+  moodsObj.mood = req.body.mood;
+  moodsObj.date = req.body.date;
+  moodsObj.notes = req.body.notes;
+  moodsObj.save((err, doc) => {
+    // If no error in inserting or creating this object, send the response object to moods/list endpoint
+    if (!err) {
+      res.redirect("moods/list");
+      // Show this message is there is an error in creating this mood object
+    } else {
+      console.log(
+        "Error during insert insertRecord function in moodsController : " + err
+      );
+    }
+  });
+}
+
+// Custom function to update a mood object
+function updateRecord(req, res) {
+  Moods.findOneAndUpdate({ _id: req.body._id }, req.body, (err, doc) => {
+    // If no error in updating this object, send the response object to moods/list endpoint
+    if (!err) {
+      res.redirect("moods/list");
+      // Show this message is there is an error in updating this mood object
+    } else {
+      console.log(
+        "Error during updateRecord function in moodsController :" + err
+      );
+    }
+  });
+}
+
+// Use the delete http (express?) method
+router.delete("/delete/:id", (req, res) => {
+  // Mongoose has inbuilt functions (including findByIdAndRemove)
+  Moods.findByIdAndRemove(req.params.id, (err, doc) => {
+    if (!err) {
+      // Send back a status 200
+      res.send(200);
+    } else {
+      console.log("Error during delete method update :" + err);
+    }
+  });
+});
+
+module.exports = router;
